e5b1d0cb080432be3a6ce946cab515b473ff205f
David <dwoo1016@gmail.com>
2021-10-31 21:02:41 -0400

    homework submit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const data = require('./data');
+const mainPost = require('./mainPost');
+const postDetails = require('./postDetails');
+const error = require('./error')
+
+// Middleware functions
+app.use(morgan('dev'));
+app.use(express.static('public'));
+
+// Express Pipeline - Main Post Request
+app.get('/', (req, res) => {
+    res.send(mainPost());
+})
+
+// Express Pipeline - Post Details Request
+app.get('/posts/:id', (req, res) => {
+    const player = data.find(req.params.id);
+    if (!player.id) res.status(404).send(error())
+    else res.send(postDetails(player))
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+})
+
